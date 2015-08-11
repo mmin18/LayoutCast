482c3ce4dee1fcd16c50c5c4ed8bea7e8cffa55b
Tu Yimin <mmin18@gmail.com>
2015-08-11 16:33:39 +0800

    auto update cast.py from github when plugin startup

--- ide/IDEAPlugin/src/com/github/mmin18/layoutcast/ide/StartupComponent.java
+++ ide/IDEAPlugin/src/com/github/mmin18/layoutcast/ide/StartupComponent.java
@@ -3,23 +3,28 @@ package com.github.mmin18.layoutcast.ide;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.extensions.PluginId;
+import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Date;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Created by mmin18 on 7/29/15.
  */
 public class StartupComponent implements ApplicationComponent {
-    public static byte[] data;
+    private static File UPDATE_FILE;
+    private static byte[] UPDATE_DATA;
+    private static double UPDATE_VER;
+    private static File STROKE_FILE;
+    private static double STROKE_VER;
 
     public StartupComponent() {
     }
@@ -29,7 +34,13 @@ public class StartupComponent implements ApplicationComponent {
             @Override
             public void run() {
                 try {
-                    URL url = new URL("https://raw.githubusercontent.com/mmin18/LayoutCast/master/cast.py");
+                    URL url;
+                    if (TimeZone.getDefault().getRawOffset() == 8 * 3600 * 1000) {
+                        // +8000 is probably china
+                        url = new URL("http://7xkzs8.com1.z0.glb.clouddn.com/cast.py");
+                    } else {
+                        url = new URL("https://raw.githubusercontent.com/mmin18/LayoutCast/master/cast.py");
+                    }
                     InputStream ins = url.openConnection().getInputStream();
                     ByteArrayOutputStream bos = new ByteArrayOutputStream();
                     byte[] buf = new byte[4096];
@@ -39,13 +50,17 @@ public class StartupComponent implements ApplicationComponent {
                     }
                     ins.close();
                     bos.close();
-                    String str = new String(bos.toByteArray(), "utf-8");
+                    byte[] data = bos.toByteArray();
+                    String str = new String(data, "utf-8");
                     Pattern p = Pattern.compile("^__plugin__\\s*=\\s*['\"](\\d+)['\"]", Pattern.MULTILINE);
                     Matcher m = p.matcher(str);
                     if (m.find()) {
                         String s = m.group(1);
                         if ("1".equals(s)) {
-                            data = bos.toByteArray();
+                            ByteArrayInputStream bis = new ByteArrayInputStream(data);
+                            UPDATE_VER = CastAction.readVersion(bis);
+                            bis.close();
+                            UPDATE_DATA = data;
                         }
                     }
                 } catch (Exception e) {
@@ -61,5 +76,74 @@ public class StartupComponent implements ApplicationComponent {
     @NotNull
     public String getComponentName() {
         return "StartupComponent";
+    }
+
+
+    public static double getCastVersion() {
+        if (STROKE_VER == 0) {
+            try {
+                File cp = new File(PathUtil.getJarPathForClass(StartupComponent.class));
+                if (cp.isDirectory()) {
+                    File file = new File(cp, "cast.py");
+                    InputStream ins = new FileInputStream(file);
+                    STROKE_VER = CastAction.readVersion(ins);
+                    ins.close();
+                } else {
+                    ZipFile zf = new ZipFile(cp);
+                    ZipEntry ze = zf.getEntry("cast.py");
+                    InputStream ins = zf.getInputStream(ze);
+                    STROKE_VER = CastAction.readVersion(ins);
+                    ins.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return Math.max(STROKE_VER, UPDATE_VER);
+    }
+
+    public static File getCastFile() {
+        getCastVersion();
+        if (STROKE_VER > UPDATE_VER) {
+            if (STROKE_FILE == null) {
+                try {
+                    File cp = new File(PathUtil.getJarPathForClass(StartupComponent.class));
+                    if (cp.isDirectory()) {
+                        STROKE_FILE = new File(cp, "cast.py");
+                    } else {
+                        ZipFile zf = new ZipFile(cp);
+                        ZipEntry ze = zf.getEntry("cast.py");
+                        InputStream ins = zf.getInputStream(ze);
+                        File tmp = File.createTempFile("lcast", "" + STROKE_VER);
+                        FileOutputStream fos = new FileOutputStream(tmp);
+                        ins = zf.getInputStream(ze);
+                        byte[] buf = new byte[4096];
+                        int l;
+                        while ((l = ins.read(buf)) != -1) {
+                            fos.write(buf, 0, l);
+                        }
+                        fos.close();
+                        ins.close();
+                        STROKE_FILE = tmp;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return STROKE_FILE;
+        } else {
+            if (UPDATE_FILE == null) {
+                try {
+                    File tmp = File.createTempFile("lcast", "" + UPDATE_VER);
+                    FileOutputStream fos = new FileOutputStream(tmp);
+                    fos.write(UPDATE_DATA);
+                    fos.close();
+                    UPDATE_FILE = tmp;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return UPDATE_FILE;
+        }
     }
 }
