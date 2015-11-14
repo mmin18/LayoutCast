88e2ddb15cdf57475442c775bb86598f953c8743
Tu Yimin <mmin18@gmail.com>
2015-11-15 00:09:29 +0800

    new layoutcast.gradle

--- sample-androidstudio/CastGradleTest/src/main/java/com/github/mmin18/layoutcast/gradle/MyApplication.java
+++ sample-androidstudio/CastGradleTest/src/main/java/com/github/mmin18/layoutcast/gradle/MyApplication.java
@@ -2,8 +2,6 @@ package com.github.mmin18.layoutcast.gradle;
 
 import android.app.Application;
 
-import com.github.mmin18.layoutcast.LayoutCast;
-
 /**
  * Created by mmin18 on 8/4/15.
  */
@@ -11,9 +9,5 @@ public class MyApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
-		if (BuildConfig.DEBUG) {
-			LayoutCast.init(this);
-		}
 	}
 }
